3f2ad37926a3ece6ddcd42a2f6dfb5f574f667a9
wuid-0801 <wuid0801@gmail.com>
2025-06-17 10:48:15 +0900

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          계정 테스트
+          계정 테스트 wuid-0801
         </p>
 
       </header>
