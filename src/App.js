3427479691a4577d22f73e26c7d7308e124500f9
Wuid0801 <pig10326@naver.com>
2025-06-17 10:46:02 +0900

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Test 22222
+          계정 테스트
         </p>
 
       </header>
