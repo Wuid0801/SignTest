31af1ef2565c6076ef69882402a89f57a1d836fa
Wuid0801 <pig10326@naver.com>
2025-06-17 16:00:32 +0900

    Test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          깃허브 이름 변경 테스트트
+          깃허브 이름 변경 테스트 2
         </p>
 
       </header>
